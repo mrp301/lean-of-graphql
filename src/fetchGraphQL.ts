06d8cb9989311d0f4c6f880c10d4fa54491a50cc
hishinuma616 <dnbwoodleaves3616@gmail.com>
2022-01-28 02:01:48 +0900

    setting-relay-compiler

---
+++ src/fetchGraphQL.ts
@@ -0,0 +1,21 @@
+const fetchGraphQL = async (text: string, variables = {}) => {
+  const REACT_APP_GITHUB_AUTH_TOKEN =
+    process.env.REACT_APP_GITHUB_AUTH_TOKEN;
+
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
+};
+
+export default fetchGraphQL;
